1560f15bb75a534d906c2ca9bab3a4e2b1b551c5
Jayson Kesterson <jayson.kesterson24@gmail.com>
2020-05-09 15:14:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function updateDriverWithKeyAndValue(obj, key, value) {
   newObj[key] = value;
   return newObj;
 }
+
+function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
+  obj[key] = value
+}
